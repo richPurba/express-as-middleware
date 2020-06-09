55e2a048d46c261eea86e15b91211921c3df5154
richpurba <tepetrol@yahoo.com>
2020-06-09 11:04:49 +0200

    adding userId resources and redis

--- README.md
+++ README.md
@@ -3,5 +3,8 @@
 ## WHAT
 This is a routing processes for Express for front-end developers. Some of the routing libs from Angular/React are less scalable, especially in managing loads of requests.
 
+## Building Express from Scratch
+Please follow [these instructions](https://expressjs.com/en/starter/hello-world.html), very easy to follow.
+
 ## Redis
 To use the redis, you should run the following command under directory [cache](cache): ```docker run --name redis-for-express -p 6379:6379 -d  redis-for-express``` The data is bound under data/folder. See [Dockerfile](cache/Dockerfile) for more detail. The redis.conf is configured such that the protection mode is off and binding to container host (not your localhost) is off.
--- routes/users.js
+++ routes/users.js
@@ -1,30 +1,36 @@
 var express = require('express');
 var router = express.Router();
-
 var {v1: uuidv1} = require('uuid');
 var redis = require('redis');
-var redisClient = redis.createClient();// doesn't work for
+var redisClient = redis.createClient();
+
+
+var collections = {
+  Rich: 123,
+  Alessandro: 455,
+  Martin: 789
+}
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  var body =  req.body;
-  var answer = "respond with a resource";
-
-  redisClient.hkeys(body.username,(err,replies) => {
-    console.log(replies);
-    res.send(replies);
-    redisClient.quit();
-  });
-
-  redisClient.set(body.username,answer,redis.print);// cached but you need to provide some mechanism to call the cache next time, instead of this one.
-  res.send(answer);
-  redisClient.quit();
+  res.send(collections);
 });
 
 /* GET user by id*/
-router.get('/:userid', function(request,response){ //NOTE: see that the req/request are 2 of the same thing!
-  response.send('user')
-}) 
+router.get('/:userId', function(request,response){ //NOTE: see that the req/request are 2 of the same thing!
+
+  // Trying to cache
+  redisClient.get(request.params.userId.toLowerCase(), (err,reply) => {
+     if(reply === null){
+      redisClient.set(request.params.userId.toLowerCase(),collections[request.params.userId]);
+     }
+  });
+  var answer = {};
+  redisClient.get(request.params.userId.toLowerCase(),(err,reply) => {
+    answer[request.params.userId] =  reply;
+    response.send(answer);
+  });
+});
 
 
 module.exports = router;
